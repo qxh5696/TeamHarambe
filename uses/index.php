06dbd97a77116511d4696dafca7e90dc2951e574
Oswaldo R <oar3327@rit.edu>
2016-11-12 12:42:17 -0500

    added uses directory and index.php file

---
+++ uses/index.php
@@ -0,0 +1,11 @@
+<?php
+    $page_title = "Uses";
+    $active_page = "uses";
+    include "assets/includes/header.php";
+    include "assets/includes/global-nav.php";
+?>
+
+
+<?php 
+    include "assets/includes/footer.php";
+?>       

692909cc44f3cce36b8b875a152836ecc0c6a570
Qadir Haqq <Haqq.Q173@gmail.com>
2016-11-19 14:27:26 -0500

    Beginning to add extra files

---
+++ header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title><?= $page_title?> | Regular Expressions</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+        <!--<link rel="stylesheet" href="css/main.css"> -->
+    </head>
+    <body>

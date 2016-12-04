1c729914cf5dcc00f95ddff42195a0512c383ee1
Henry Kirk <henry@the-kirks.com>
2016-12-04 14:18:49 -0500

    Moved scripts from header to footer

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -1,6 +1,12 @@
 		</div> <!-- /col-sm-9 offset-sm-3 col-md-10 offset-md-2 main (opened in header) -->
+       
+        <!-- jQuery -->
         <script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.4.min.js"><\/script>')</script>
+
+        <!-- Bootstrap JS -->
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/npm.js"></script>
+        <!-- <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.4.min.js"><\/script>')</script> -->
 
         <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
         <script>
@@ -8,5 +14,6 @@
             ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
         </script>
         <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+        <script src="assets/js/fillSidebar.js"></script>
     </body>
 </html>
--- assets/includes/header.php
+++ assets/includes/header.php
@@ -14,14 +14,9 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
 
-        <!-- jQuery -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
         <!-- Bootstrap -->
         <link href="assets/css/bootstrap.css" rel="stylesheet">
         <link href="assets/css/bootstrap-theme.min.css" rel="stylesheet">
-        <script src="assets/js/bootstrap.min.js"></script>
-        <script src="assets/js/npm.js"></script>
 
         <!-- Custom styles for this site -->
         <link href="assets/css/nav.css" rel="stylesheet">
